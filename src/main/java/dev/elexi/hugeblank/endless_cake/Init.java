9918b37543928b1a3d29169fb1e2696445985aed
hugeblank <hugeblank@gmail.com>
2022-04-07 03:38:58 -0700

    Bring Endless Cake to 1.18
    closes #1

--- src/main/java/dev/elexi/hugeblank/endless_cake/Init.java
+++ src/main/java/dev/elexi/hugeblank/endless_cake/Init.java
@@ -27,34 +27,30 @@ public class Init implements ModInitializer {
         }
     }
 
-    private static void registerBlock(String name, Block block) {
-        Identifier id = new Identifier(ID, name);
-        Registry.register(Registry.BLOCK, id, block);
-    }
-
     private static void registerCakeType(String name, EndlessCakeBlock cake) {
         registerBlock(name, cake, ItemGroup.FOOD);
-        registerBlock("candle_" + name, new EndlessCandleCakeBlock(Blocks.CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CAKE).luminance((state) -> (Boolean)state.get(Properties.LIT) ? 3 : 0)));
-        registerBlock("white_candle_" + name, new EndlessCandleCakeBlock(Blocks.WHITE_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)));
-        registerBlock("orange_candle_" + name, new EndlessCandleCakeBlock(Blocks.ORANGE_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)));
-        registerBlock("magenta_candle_" + name, new EndlessCandleCakeBlock(Blocks.MAGENTA_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)));
-        registerBlock("light_blue_candle_" + name, new EndlessCandleCakeBlock(Blocks.LIGHT_BLUE_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)));
-        registerBlock("yellow_candle_" + name, new EndlessCandleCakeBlock(Blocks.YELLOW_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)));
-        registerBlock("lime_candle_" + name, new EndlessCandleCakeBlock(Blocks.LIME_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)));
-        registerBlock("pink_candle_" + name, new EndlessCandleCakeBlock(Blocks.PINK_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)));
-        registerBlock("gray_candle_" + name, new EndlessCandleCakeBlock(Blocks.GRAY_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)));
-        registerBlock("light_gray_candle_" + name, new EndlessCandleCakeBlock(Blocks.LIGHT_GRAY_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)));
-        registerBlock("cyan_candle_" + name, new EndlessCandleCakeBlock(Blocks.CYAN_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)));
-        registerBlock("purple_candle_" + name, new EndlessCandleCakeBlock(Blocks.PURPLE_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)));
-        registerBlock("blue_candle_" + name, new EndlessCandleCakeBlock(Blocks.BLUE_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)));
-        registerBlock("brown_candle_" + name, new EndlessCandleCakeBlock(Blocks.BROWN_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)));
-        registerBlock("green_candle_" + name, new EndlessCandleCakeBlock(Blocks.GREEN_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)));
-        registerBlock("red_candle_" + name, new EndlessCandleCakeBlock(Blocks.RED_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)));
-        registerBlock("black_candle_" + name, new EndlessCandleCakeBlock(Blocks.BLACK_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)));
+        registerBlock("candle_" + name, new EndlessCandleCakeBlock(Blocks.CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CAKE).luminance((state) -> (Boolean)state.get(Properties.LIT) ? 3 : 0)), null);
+        registerBlock("white_candle_" + name, new EndlessCandleCakeBlock(Blocks.WHITE_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)), null);
+        registerBlock("orange_candle_" + name, new EndlessCandleCakeBlock(Blocks.ORANGE_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)), null);
+        registerBlock("magenta_candle_" + name, new EndlessCandleCakeBlock(Blocks.MAGENTA_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)), null);
+        registerBlock("light_blue_candle_" + name, new EndlessCandleCakeBlock(Blocks.LIGHT_BLUE_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)), null);
+        registerBlock("yellow_candle_" + name, new EndlessCandleCakeBlock(Blocks.YELLOW_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)), null);
+        registerBlock("lime_candle_" + name, new EndlessCandleCakeBlock(Blocks.LIME_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)), null);
+        registerBlock("pink_candle_" + name, new EndlessCandleCakeBlock(Blocks.PINK_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)), null);
+        registerBlock("gray_candle_" + name, new EndlessCandleCakeBlock(Blocks.GRAY_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)), null);
+        registerBlock("light_gray_candle_" + name, new EndlessCandleCakeBlock(Blocks.LIGHT_GRAY_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)), null);
+        registerBlock("cyan_candle_" + name, new EndlessCandleCakeBlock(Blocks.CYAN_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)), null);
+        registerBlock("purple_candle_" + name, new EndlessCandleCakeBlock(Blocks.PURPLE_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)), null);
+        registerBlock("blue_candle_" + name, new EndlessCandleCakeBlock(Blocks.BLUE_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)), null);
+        registerBlock("brown_candle_" + name, new EndlessCandleCakeBlock(Blocks.BROWN_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)), null);
+        registerBlock("green_candle_" + name, new EndlessCandleCakeBlock(Blocks.GREEN_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)), null);
+        registerBlock("red_candle_" + name, new EndlessCandleCakeBlock(Blocks.RED_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)), null);
+        registerBlock("black_candle_" + name, new EndlessCandleCakeBlock(Blocks.BLACK_CANDLE, cake, AbstractBlock.Settings.copy(Blocks.CANDLE_CAKE)), null);
     }
 
     @Override
     public void onInitialize() {
+        // Just a single cake, for now...
         registerCakeType("endless_cake", ENDLESS_CAKE);
     }
 }
--- src/main/java/dev/elexi/hugeblank/endless_cake/mixin/CandleCakeBlockMixin.java
+++ src/main/java/dev/elexi/hugeblank/endless_cake/mixin/CandleCakeBlockMixin.java
@@ -1,18 +1,38 @@
 package dev.elexi.hugeblank.endless_cake.mixin;
 
 import dev.elexi.hugeblank.endless_cake.block.EndlessCandleCakeBlock;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.CandleCakeBlock;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Map;
 
 @Mixin(CandleCakeBlock.class)
 class CandleCakeBlockMixin {
 
-	@Redirect(at = @At(value = "INVOKE", target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"), method = "<init>")
-	private <K, V> V avoidMapInsertion(Map<K, V> map, K key, V value) {
-		return value instanceof EndlessCandleCakeBlock ? null : map.put(key, value);
+	@Shadow
+	@Final
+	private static Map<Block, CandleCakeBlock> CANDLES_TO_CANDLE_CAKES;
+	private CandleCakeBlock endless_cake$cake_original;
+
+	@Inject(at = @At(value = "INVOKE", target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"), method = "<init>")
+	private void before(Block candle, AbstractBlock.Settings settings, CallbackInfo ci) {
+		endless_cake$cake_original = CANDLES_TO_CANDLE_CAKES.get(candle);
+	}
+
+	@Inject(at = @At(value = "INVOKE_ASSIGN", target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"), method = "<init>")
+	private void after(Block candle, AbstractBlock.Settings settings, CallbackInfo ci) {
+		//noinspection ConstantConditions
+		if ((Object)this instanceof EndlessCandleCakeBlock)
+			CANDLES_TO_CANDLE_CAKES.put(candle, endless_cake$cake_original);
 	}
 }
